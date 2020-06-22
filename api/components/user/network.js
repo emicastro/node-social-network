5645ae15f88bee4c8a15e08319c6aea229bb192f
Emilio Castro <emilio.castro@mercadolibre.com>
2020-06-22 12:22:13 -0300

    Adding Swagger for API documentation

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,10 +1,55 @@
 const express = require('express')
 
 const response = require('../../../network/response')
+const Controller = require('./index.js')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  response.success(req, res, 'Ok', 200)
+// Routes
+router.get('/', getRoutes)
+router.get('/:id', getRoute)
+router.post('/', upsert)
+router.put('/:id', upsert)
+
+// Internal function
+function getRoutes(req, res) {
+  Controller.list()
+    .then((usersList) => {
+      response.success(req, res, usersList, 200)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
+}
+
+function getRoute(req, res) {
+  Controller.get(req.params.id)
+    .then((user) => {
+      response.success(req, res, user, 200)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
+}
+
+function upsert(req, res) {
+  Controller.upsert(req.body)
+    .then((user) => {
+      response.success(req, res, user, 201)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
+}
+
+router.delete('/:id', (req, res) => {
+  Controller.remove(req.params.id)
+    .then((userId) => {
+      response.success(req, res, userId, 200)
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500)
+    })
 })
 
 module.exports = router
--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const swaggerUI = require('swagger-ui-express')
 
 const config = require('../config')
 const user = require('./components/user/network')
 
 const app = express()
+const swaggerDoc = require('./swagger.json')
+
+app.use(bodyParser.json())
 
 // ROUTER
 app.use('/api/user', user)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
 app.listen(config.api.port, () => {
   console.log('API listening in port ', config.api.port)
