a4381ab002a51a50c503e2738e0264041494b488
Emilio Castro <hello@emicastro.com>
2020-08-13 20:22:44 -0300

    Adds Redis chace microservice

--- config.js
+++ config.js
@@ -15,8 +15,17 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || ''
   },
+  redis: {
+    host: process.env.REDIS_HOST || '',
+    port: process.env.REDIS_PORT || '',
+    password: process.env.REDIS_PASSWORD || ''
+  },
   mysqlService: {
     host: process.env.MYSQL_SRV_HOST || 'localhost',
     port: process.env.MYSQL_SRV_PORT || 3001
+  },
+  cacheService: {
+    host: process.env.CACHE_SRV_HOST || 'localhost',
+    port: process.env.CACHE_SRV_PORT || 3003
   }
 }
